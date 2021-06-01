70e13b0adf21ce3c4ce5dd290ad76f16f87d19fa
Вячеслав Жубрин <vyacheslavzhubrin@gmail.com>
2021-06-01 18:18:10 +0300

    исправил

--- schema.sql
+++ schema.sql
@@ -5,38 +5,45 @@ CREATE DATABASE yeticave
 USE yeticave;
 
 CREATE TABLE categories (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(30),
   code VARCHAR(10)
 );
 
 CREATE TABLE lots (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  lot_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   date_add DATETIME,
   name VARCHAR(30),
   description TEXT(100),
   img VARCHAR(50),
   starting_price INT,
   date_end DATETIME,
-  bet_step INT
+  bet_step INT,
+  user_id INT NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(user_id),
+  FOREIGN KEY(category_id) REFERENCES categories(category_id)
 );
 
 CREATE TABLE bets (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  bet_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   date_add DATETIME,
-  sum INT
+  sum INT,
+  user_id INT NOT NULL,
+  lot_id INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(user_id),
+  FOREIGN KEY(lot_id) REFERENCES lots(lot_id)
 );
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   registration_date DATETIME,
   email VARCHAR(50) UNIQUE,
   name VARCHAR(30),
   password VARCHAR(20) UNIQUE,
-  contacts TEXT(100)
+  contacts TEXT(100),
+  lot_id INT NOT NULL,
+  bet_id INT NOT NULL,
+  FOREIGN KEY(lot_id) REFERENCES lots(lot_id),
+  FOREIGN KEY(bet_id) REFERENCES bets(bet_id)
 );
-
-SELECT * FROM lots JOIN categories;
-SELECT * FROM lots JOIN users;
-SELECT * FROM bets JOIN lots;
-SELECT * FROM bets JOIN users;
